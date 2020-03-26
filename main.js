80a320a4d14896472e88e568532f62ec4cd5c118
Prosper Vean <prospervean@gmail.com>
2020-03-26 11:58:31 -0700

    first

---
+++ main.js
@@ -0,0 +1,8 @@
+let name = "chukwuma prosper";
+let course = ["html", "css", "javascript", "php"];
+
+console.log("my name is:" + name + " and  my courses are:" + course);
+
+for (let i = 1; i < 202; i++) {
+    if (i % 2===0) console.log(i)
+}
